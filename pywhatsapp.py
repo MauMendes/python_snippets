ddb9ce5ec9759e64516185a3a2a7a4bddd4c77b3
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-07-08 13:22:09 +1200

    pywhatsapp send msg, but needs to be logged in using web.whatsapp

---
+++ pywhatsapp.py
@@ -0,0 +1,8 @@
+#pip install pywhatkit
+#C:\Python38\Scripts\pip.exe install pywhatkit
+import pywhatkit as kit
+
+phone_number = xxxxxxx
+kit.sendwhatmsg_instantly(f"+64{phone_number}", "teste 123", 14, 3)
+
+print('done')
