01314ab1ca1955ec4105521f1fcdc1e99dadd058
Mauricio Mendes <mauricio.eletrica@gmail.com>
2022-07-11 18:23:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,150 @@
+# //Tutorial 1 - print
+# print("Hello World")
+# print('o----')
+# print(' ||||')
+# rint( '/' + '*' * 10)
+
+# Tutorial 2 - variables
+# price = 10
+# rating = 4.9
+# name = "mauricio"
+# is_published = True
+# print(price)
+# price = 20
+# print(price)
+# #Example
+# pacient_name = "John Smith"
+# pacient_age = 20
+# paciente_new = True
+
+#Tutorial 3 - input
+# name = input('What is your name? ')
+# # print('Hi Ma' + name)
+# #Example
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print( name + ' favorite color is ' + color)
+
+#Tutorial 4 - input
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(age)
+# print(type(age))
+#Example
+# weight_pounds = input('What is your Weight in Pounds? ')
+# weight_kg = float(weight_pounds) * 0.453592
+# print('Your waight in Kg is :' + str(weight_kg))
+
+#Tutorial 5
+# name = 'Jennifer'
+# print(name[1:-1])
+
+#Tutorial 6 - Format Strings
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+#
+# course = 'Python for beginners'
+# print(len(course))
+# course.upper()
+# print(course.upper())
+# print(course.find('beginners'))
+# print(course.replace('beginners', 'Absolute beginners'))
+# print('Python' in course)
+
+#Tutorial 7 - floats
+
+#Tutorial 7 if
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print('We have a hot day')
+# elif is_cold:
+#     print('We have a cold day')
+# else:
+#     print('No we dont')
+# Exercise
+# price = 1E6
+# has_good_credit = True
+# good_criminal_record = True
+#
+# if has_good_credit and good_criminal_record:
+#     house_price = price - (price * 0.10)
+# else:
+#     house_price = price - (price * 0.20)
+# print(f"house price {house_price}")
+#
+# name = input("input your name ")
+# name_size = len(name)
+# if name_size < 3:
+#     print("name must be at least 3 characters")
+# elif name_size > 50:
+#     print("name can be a maximum of 50 characters")
+# else:
+#     print("name looks good")
+
+#Tutorial While loops
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+# Exercise
+# command = ""
+# while True:
+#     command = input("< ").lower()
+#     if command == "start":
+#         print("Car started... read to go!")
+#     elif command == "stop":
+#         print("Car started... read to go!")
+#     elif command == "help":
+#         print("available commands: help, start, stop, quit")
+#     elif command == "quit":
+#         break
+#     else:
+#         print("I dont Understand that!")
+#
+# print("thanks for playing")
+
+#Tutorial For Loops
+# for item in ['Mauricio', 'Marcelo', 'Eva']:
+#     print(item)
+# for item in range(5,10,2):
+#     print(item)
+# prices = [10, 20, 30]
+# total = 0
+# for price in prices:
+#     total += price
+# print(f"Total Price: {total}")
+
+# for x in range(4):
+#     for y in range(3):
+#         print( f'({x},{y})')
+
+# numbers = [5, 2, 5, 2, 2]
+# for item in numbers:
+#     output = ""
+#     for count in range(item):
+#         output += 'x'
+#     print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
